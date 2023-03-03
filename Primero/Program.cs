43547b234ca595436d0474ee304d5dad8ffe9a68
Xavier9898 <104702367+Xavier9898@users.noreply.github.com>
2023-03-03 14:12:58 -0600

    se agrega el proyecto

---
+++ Primero/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Primero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hazlo facil, haz Click!");
+        }
+    }
+}
