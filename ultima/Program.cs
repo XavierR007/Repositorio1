d7fd647898ff0ccb17ccf35fb421acca19911a39
XavierR007 <126616105+XavierR007@users.noreply.github.com>
2023-03-03 14:58:16 -0600

    Add files via upload

---
+++ ultima/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace ultima
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+
+            using (TextWriter w = File.CreateText("log.txt"));
+            {
+                w.WriteLine("Linea uno");
+                w.WriteLine("Linea dos");
+            }
+
+            using (TextReader r = File.OpenText("log.txt"));
+            {
+                while ((s = r.ReadLine()) != null) 
+                {
+                    Console.WriteLine(s);
+                }
+            }
+        }
+    }
+}
